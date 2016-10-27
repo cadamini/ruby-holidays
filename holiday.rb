70594d01b86fc6ca628189478c43055018291aff
Christian Adamini <christian.adamini@invision.de>
2016-10-27 19:36:59 +0200

    Remove sample code from holiday.rb

--- holiday.rb
+++ holiday.rb
@@ -111,33 +111,3 @@ class TemplateFileGenerator
     end
   end
 end
-
-# main program example
-
-regions = {
-  deutschland:
-  {
-    de: 'Gesamt',
-    de_sn: 'Sachsen',
-    de_bb: 'Brandenburg',
-    de_bw: 'Baden-Württemberg',
-    de_by: 'Bayern',
-    de_he: 'Hessen',
-    de_mv: 'Mecklenburg-Vorpommern',
-    de_nw: 'Nordrhein-Westfalen',
-    de_rp: 'Rheinland-Pfalz',
-    de_sl: 'Saarland',
-    de_st: 'Sachsen-Anhalt',
-    de_th: 'Thürigen'
-  },
-  frankreich:
-  {
-    fr: 'Gesamt'
-  }
-}
-
-result = TemplateFileGenerator.new([2016, 2017], regions).run
-result.each do |output|
-  output.to_console
-  output.to_archive
-end
